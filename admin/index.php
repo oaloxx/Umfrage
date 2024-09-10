8da89d9f84f86e9b134f396c7bf2d41229cf58cf
Mohd <104264886+oaloxx@users.noreply.github.com>
2024-09-10 09:22:25 +0200

    index.php

---
+++ admin/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8" />
+  <title>Umfrage - Adminbereich</title>
+</head>
+<body>
+<h1>Umfrage - Adminbereich</h1>
+
+<div style="font-size:30px;color:red;">Dieser Bereich ist schon programmiert! Bitte hier nichts programmieren!</div>
+
+</body>
+</html>
