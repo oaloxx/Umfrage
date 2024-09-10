4a47f3ae9b69a3a9f756bf87834e27cd9ff55d35
Mohd <104264886+oaloxx@users.noreply.github.com>
2024-09-10 09:21:27 +0200

    Add files via upload

---
+++ dev/testdaten.sql
@@ -0,0 +1,16 @@
+insert into frage(fragentext) values
+('Was war die Farbe des weißen Rosses Heinrichs IV.?'),
+('Wer sagte \"Sammelt euch um meinen weißen Panasch\"?'),
+('Was war Ravaillac?');
+
+
+insert into moeglicheantwort(frageid,antworttext) values
+(1,'Blau'),
+(1,'Grau'),
+(1,'Weiß'),
+(2,'Heinrich IV.'),
+(2,'Friedrich I.'),
+(2,'Sankt Aloysius'),
+(3,'Eine amerikanische Automarke'),
+(3,'Der Mörder Heinrichs IV.'),
+(3,'Ein berühmter Ort bei Bordeaux');
